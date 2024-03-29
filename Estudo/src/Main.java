21582fc661952b9b7ef7f8fc3de81a066addcb24
gregorystevao19 <contato.gregorystevao@gmail.com>
2024-03-29 13:29:23 -0300

    feat: projeto geral para estudos e exemplos

---
+++ Estudo/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 10;
+        int resultado = soma(num1, num2);
+
+        System.out.println(resultado);
+
+
+    }
+    public static int soma(int a, int b){
+        return a + b;
+    }
+}
